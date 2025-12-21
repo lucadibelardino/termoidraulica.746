afec51b9674ff8e1ca26678114413d647eaf227a
lucadibelardino <luca.dibelardino@gmail.com>
2025-12-21 15:22:14 +0100

    Add basename to Router for GitHub Pages subdirectory support

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const ScrollToTop = () => {
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <div className="min-h-screen flex flex-col bg-slate-50">
         <ScrollToTop />
         <Navbar />
